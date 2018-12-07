b772890f5702ffe20f8c3b5f679ba6e0cad25f6a
nofearjoe <ilyakharabet@gmail.com>
2018-12-07 17:18:57 +0300

    WIP

---
+++ Timmee/Agile diary/Sources/Presentation layer/Modules/Authorization/AuthorizationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AuthorizationViewController.swift
+//  Agile diary
+//
+//  Created by i.kharabet on 07.12.2018.
+//  Copyright © 2018 Mesterra. All rights reserved.
+//
+
+import UIKit
+
+final class AuthorizationViewController: BaseViewController {
+    
+    
+    
+}
