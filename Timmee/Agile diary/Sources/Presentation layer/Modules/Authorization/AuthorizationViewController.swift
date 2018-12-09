f191432e379cd5148565e6da97a572bd841db098
Ilya Kharabet <ilyakharabet@gmail.com>
2018-12-09 18:49:53 +0300

    Firebase

--- Timmee/Agile diary/Sources/Presentation layer/Modules/Authorization/AuthorizationViewController.swift
+++ Timmee/Agile diary/Sources/Presentation layer/Modules/Authorization/AuthorizationViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Authorization
 
 final class AuthorizationViewController: BaseViewController {
     
---
+++ Timmee/Authorization/Other/Authorization.h
@@ -0,0 +1,19 @@
+//
+//  Authorization.h
+//  Authorization
+//
+//  Created by Илья Харабет on 09/12/2018.
+//  Copyright © 2018 Илья Харабет. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Authorization.
+FOUNDATION_EXPORT double AuthorizationVersionNumber;
+
+//! Project version string for Authorization.
+FOUNDATION_EXPORT const unsigned char AuthorizationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Authorization/PublicHeader.h>
+
+
