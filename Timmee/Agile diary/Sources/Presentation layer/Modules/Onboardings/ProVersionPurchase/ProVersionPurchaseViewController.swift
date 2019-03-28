591e9741e9eb31157def2bbc58fd0b5e32c1df75
Ilya Kharabet <i.kharabet@tinkoff.ru>
2019-03-28 17:20:06 +0300

    Change structure

---
+++ Timmee/Agile diary/Sources/Presentation layer/Modules/Onboardings/ProVersionPurchase/ProVersionPurchaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProVersionPurchaseViewController.swift
+//  Agile diary
+//
+//  Created by i.kharabet on 28/03/2019.
+//  Copyright © 2019 Mesterra. All rights reserved.
+//
+
+import UIKit
+
+final class ProVersionPurchaseViewController: BaseViewController {
+    
+    @IBOutlet private var headerView: LargeHeaderView!
+    @IBOutlet private var titleLabel: UILabel!
+    @IBOutlet private var 
+    
+}
